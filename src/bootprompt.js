7780553fe4d5bdcfa084baae13a0b64d4a3b0af8
Louis-Dominique Dubeau <ldd@lddubeau.com>
2019-02-06 10:49:47 -0500

    Drop the Object.keys polyfill.
    It is not our responsibility to provide this polyfill.

--- src/bootprompt.js
+++ src/bootprompt.js
@@ -20,50 +20,6 @@
 }(this, function init($, undefined) {
   'use strict';
 
-  //  Polyfills Object.keys, if necessary.
-  //  @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
-  if (!Object.keys) {
-    Object.keys = (function () {
-      'use strict';
-      var hasOwnProperty = Object.prototype.hasOwnProperty,
-        hasDontEnumBug = !({ toString: null }).propertyIsEnumerable('toString'),
-        dontEnums = [
-          'toString',
-          'toLocaleString',
-          'valueOf',
-          'hasOwnProperty',
-          'isPrototypeOf',
-          'propertyIsEnumerable',
-          'constructor'
-        ],
-        dontEnumsLength = dontEnums.length;
-
-      return function (obj) {
-        if (typeof obj !== 'function' && (typeof obj !== 'object' || obj === null)) {
-          throw new TypeError('Object.keys called on non-object');
-        }
-
-        var result = [], prop, i;
-
-        for (prop in obj) {
-          if (hasOwnProperty.call(obj, prop)) {
-            result.push(prop);
-          }
-        }
-
-        if (hasDontEnumBug) {
-          for (i = 0; i < dontEnumsLength; i++) {
-            if (hasOwnProperty.call(obj, dontEnums[i])) {
-              result.push(dontEnums[i]);
-            }
-          }
-        }
-
-        return result;
-      };
-    }());
-  }
-
   var exports = {};
 
   var VERSION = '1.0.0';
